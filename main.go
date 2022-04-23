2875735fd40b80dcfd876613b021b27136f29910
n8pickle <n8pickle@gmail.com>
2022-04-23 09:08:11 -0600

    removed appends and am using arrays

--- main.go
+++ main.go
@@ -39,9 +39,8 @@ func main() {
 	// Hour Stack
 	hsIndex := 0
 
-	baseArr := [127]int{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,25, 26, 27,28,29,30,31,32,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,123,124,125,126,127 }
+	baseArr := [127]int{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,25, 26, 27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,126,127 }
 	reserveQueue := baseArr[:ballCount]
-	initialOrder := baseArr[:ballCount]
 
 	
 	// rqRemIndex = (rqRemIndex + 1) % ballCount
@@ -67,7 +66,6 @@ func main() {
 					reserveQueue[rqInsIndex] = fiveMinStack[i]
 					rqInsIndex = (rqInsIndex + 1) % ballCount
 				}
-				fmIndex = 0
 				if hsIndex == 11 {
 					lastBall := hourStack[hsIndex]
 					for i := 10; i >= 0; i-- {
@@ -76,7 +74,10 @@ func main() {
 					}
 					reserveQueue[rqInsIndex] = lastBall
 					rqInsIndex = (rqInsIndex + 1) % ballCount
-					if ballsRepeat(initialOrder, reserveQueue){
+					if minutesToRepeat == 156751200 {
+						fmt.Print("ehhh")
+					}
+					if ballsRepeat(reserveQueue, ballCount){
 						break
 					}
 				}
@@ -91,19 +92,15 @@ func main() {
 	fmt.Printf("%v balls took %v days to repeat (%v elapsed)\n", ballCount, daysToRepeat, elapsed);
 }
 
-func ballsRepeat(initial []int, current []int) bool {
-	if(len(initial) == len(current)) {
-		for i := len(initial)-1; i >= 0; i-- {
-			if initial[i] == current[i] {
+func ballsRepeat(reserveQueue []int, ballCount int) bool {
+		for i := ballCount - 1; i >= 0; i-- {
+			if reserveQueue[i] == i + 1 {
 				continue
-			} else {
-				return false
+			} else { 
+				return false 
 			}
 		}
-
 		return true
-	}
-	return false
 }
 
 func createInitialArray(arr []int, ballCount int) []int {
